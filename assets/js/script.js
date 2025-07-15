7bd933dd9fed1847b53bc8357e5ffa1dd79d480f
kevlog <kevlog@users.noreply.github.com>
2025-07-15 21:16:36 +0700

    fix(js): open all links in new tab regardless of destination

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("a[href^='http']:not([target])");
+    const links = document.querySelectorAll("a[href]:not([target])");
     links.forEach(link => {
         link.setAttribute("target", "_blank");
         link.setAttribute("rel", "nofollow noopener noreferrer");
